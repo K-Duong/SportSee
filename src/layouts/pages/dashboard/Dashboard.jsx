06d3a104ea3a1d5f733a542f2ffcd0f59f6c7d03
Duong TRINH <khanhduong.dav@gmail.com>
2024-05-17 11:09:22 +0200

    redefine initial value with user Object for context

--- src/layouts/pages/dashboard/Dashboard.jsx
+++ src/layouts/pages/dashboard/Dashboard.jsx
@@ -11,31 +11,39 @@ import UserContext from "../../../store/UserContext.jsx";
 function Dashboard() {
   const { id } = useParams();
   const foundedId = Number(id);
-
-  const user = data.USER_MAIN_DATA.find((d) => d.id === foundedId);
-  console.log(Object.entries(user.keyData));
+  // TODO: toi uu hoa ?
+  const user = {
+    id: foundedId,
+    mainData: data.USER_MAIN_DATA.find((d) => d.id === foundedId),
+    activity: data.USER_ACTIVITY.find((d) => d.userId === foundedId).sessions,
+    performanceInfos: data.USER_PERFORMANCE.find((d) => d.userId === foundedId),
+    averageSession: data.USER_AVERAGE_SESSIONS.find(
+      (d) => d.userId === foundedId
+    ),
+  };
+  console.log(user);
 
   return (
-    <UserContext.Provider value={foundedId}>
-      
-          <div className="dashboard-container">
-            <header className="dashboard-header">
-              <h1>
-                Bonjour{" "}
-                <span className="user-name">{user.userInfos.firstName}</span>{" "}
-              </h1>
-              <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
-            </header>
-            <div className="dashboard-content">
-              <div className="charts"></div>
-              <div className="values-consumption">
-                {Object.entries(user.keyData).map((arr) => (
-                  <Value key={arr[0]} arr={arr} />
-                ))}
-              </div>
-            </div>
+    <UserContext.Provider value={user}>
+      <div className="dashboard-container">
+        <header className="dashboard-header">
+          <h1>
+            Bonjour{" "}
+            <span className="user-name">
+              {user.mainData.userInfos.firstName}
+            </span>{" "}
+          </h1>
+          <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+        </header>
+        <div className="dashboard-content">
+          <div className="charts"></div>
+          <div className="values-consumption">
+            {Object.entries(user.mainData.keyData).map((arr) => (
+              <Value key={arr[0]} arr={arr} />
+            ))}
           </div>
-
+        </div>
+      </div>
     </UserContext.Provider>
   );
 }
