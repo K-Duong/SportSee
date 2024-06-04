332a5918318669d9b20efde73a2ef4a1dda7f44c
Duong TRINH <khanhduong.dav@gmail.com>
2024-06-04 10:19:33 +0200

    update useEffect

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -2,7 +2,9 @@ import UserContext from "./UserContext";
 import ErrorContent from "../components/error/errorContent";
 import LoadingContent from "../components/loading/loadingContent";
 import { useState, useEffect } from "react";
-// import { getUserInfos, getUserActivity, getUserPerform, getUserAverageSession } from "../services/api_mock.js"
+
+////cette ligne vous permet d'utiliser le data mocké.
+// import { getUserInfos, getUserActivity, getUserPerform, getUserAverageSession } from "../services/api_mock.js" 
 import { getUserInfos, getUserActivity, getUserPerform, getUserAverageSession } from "../services/api";
 
 function UserProvider({ children }) {
@@ -11,52 +13,44 @@ function UserProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  const fetchApi = async () => {
-    try {
-      const mainData = await getUserInfos(id);
-      const activities = await getUserActivity(id);
-      const performanceInfos= await getUserPerform(id);
-      const averageSession = await getUserAverageSession(id);
-
-      setUser((prev) => {
-        return {
-          ...prev,
-          id: id,
-          mainData,
-          activities,
-          performanceInfos,
-          averageSession,
-        };
-      });
-    } catch (err) {
-      console.log(err);
-      setError(() => {
-        return {
-          status: err.response.status,
-          message: "Oops, une erreur est survenue 🫨. Réessayez plus tard !",
-        };
-      });
-    } finally {
-      setLoading(false);
-    }
-  };
-
+  
   useEffect(() => {
-    //TODO: améliorer avec un state et les conditions 
-    let ignore = false;
-    setUser(null);
-    setError(null)
-
-    if (!ignore) {
-      fetchApi();      
-    }
-    // clean up function
-    return () => {
-      ignore = true;
+    const fetchApi = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const mainData = await getUserInfos(id);
+        const activities = await getUserActivity(id);
+        const performanceInfos= await getUserPerform(id);
+        const averageSession = await getUserAverageSession(id);
+        setUser((prev) => {
+          return {
+            ...prev,
+            id: id,
+            mainData,
+            activities,
+            performanceInfos,
+            averageSession,
+          };
+        });
+  
+      } catch (err) {
+        console.log(err);
+        setUser(null)
+        setError(() => {
+          return {
+            status: err.response.status,
+            message: "Oops, une erreur est survenue 🫨. Réessayez plus tard !",
+          };
+        });
+      } finally {
+        setLoading(false);
+      }
     };
-  }, []);
-  // console.log(user);
+    // console.log("mounted")
+    fetchApi();
+  }, [setUser])
+
 
   return (
     <UserContext.Provider value={{ user }}>
